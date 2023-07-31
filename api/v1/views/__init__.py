110b86e5222792e5360fb2f5b68bae66dbf0b303
kaytee07 <kofi.taylor07@gmail.com>
2023-07-31 09:40:34 +0000

    add

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,9 +3,15 @@
 blueprint
 """
 from flask import Blueprint
-
+from models.state import State
+from models.city import City
+from models import storage
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 app_views = Blueprint('api_views', __name__, url_prefix='/api/v1')
 
 from .index import *
 from .states import *
-from .cities import *
