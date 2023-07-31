bc1a53142b4c63cfa49d44e939583bce3cc8cdd4
kaytee07 <kofi.taylor07@gmail.com>
2023-07-31 10:12:47 +0000

    state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -29,7 +29,8 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def del_state(state_id):
     """
     delete state using state id passed in the the uri
